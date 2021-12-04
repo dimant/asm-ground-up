134e15eaf6f1b11261edb0b044f852fd2efc1661
Diman Todorov <diman.todorov@outlook.com>
2021-12-04 15:43:10 -0800

    l8 ex3

---
+++ 1-lesson8-ex2/main.s
@@ -0,0 +1,21 @@
+
+
+		AREA Carry, CODE, READONLY
+		ENTRY
+		EXPORT __main
+			
+			
+__main
+		LDR		R0, =0xF62562FA
+		LDR		R1, =0xF412963B
+		
+		MOV		R2, #0x35
+		MOV		R3, #0x21
+		
+		ADDS	R5, R1, R0
+		ADC		R6, R2, R3 
+		ADDS	R5, R2, R3
+
+Stop	B		Stop
+
+		END
