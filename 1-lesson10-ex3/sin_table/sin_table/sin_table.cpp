d0a8b595f8255599b6a3ec664bb148c9a2b70d33
Diman Todorov <diman.todorov@outlook.com>
2021-12-06 19:51:37 -0800

    sin

--- 1-lesson10-ex3/sin_table/sin_table/sin_table.cpp
+++ 1-lesson10-ex3/sin_table/sin_table/sin_table.cpp
@@ -5,10 +5,16 @@
 #define _USE_MATH_DEFINES // for C
 #include <math.h>
 
+#include <stdint.h>
+
 int main()
 {
     float j[92];
-    float sin_val;
+    union f_u {
+        float f;
+        uint32_t u;
+    } sin_val, sv1, sv2, sv3, sv4;
+
     FILE* fp;
 
     if ((fp = fopen("sindata.txt", "w")) == 0)
@@ -19,15 +25,22 @@ int main()
 
     for (int i = 0; i < 92; i++)
     {
-        sin_val = (float) sin(M_PI * i / 180.0);
-        j[i] = sin_val;
+        sin_val.f = (float) sin(M_PI * i / 180.0);
+        j[i] = sin_val.f;
     }
 
     for (int i = 0; i < 92; i += 4)
     {
-        fprintf(fp, "DCD\t %f, %f, %f, %f\n", 
-            j[i], j[i + 1], j[i + 2], j[i + 3]);
+        sv1.f = j[i];
+        sv2.f = j[i + 1];
+        sv3.f = j[i + 1];
+        sv4.f = j[i + 1];
+
+        fprintf(fp, "DCD\t 0x%x, 0x%x, 0x%x, 0x%x\n", 
+            sv1.u, sv2.u, sv3.u, sv4.u);
     }
+
+    fclose(fp);
 
     return 0;
 }
